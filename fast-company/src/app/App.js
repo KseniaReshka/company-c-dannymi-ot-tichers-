7d72ce40fc71134e77eec84e9dc7fc29ee7d2272
Ksenia Reshetkina <ksuxa_me@mail.ru>
2023-03-03 14:53:58 +0300

    k

--- fast-company/src/app/App.js
+++ fast-company/src/app/App.js
@@ -13,7 +13,7 @@ function App() {
             <NavBar />
             <Switch>
             {/* <Route path="/users/:userId?/" component={UserForm} /> */}
-                <Route path="/users/:userId?:edit?" component={Users} />
+                <Route path="/users/:userId?/:edit?" component={Users} />
                 <Route path="/login/:type?" component={Login} />
                 <Route path="/" exact component={Main} />
                 <Redirect to="/" />
