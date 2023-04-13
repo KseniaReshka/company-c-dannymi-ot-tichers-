f3cbaa168088308b021c71c6b5bc32359b8f7d86
Ksenia Reshetkina <ksuxa_me@mail.ru>
2023-04-13 12:42:54 +0500

    с редактированием стр пользователя

--- fast-company/src/app/components/ui/qualities/qualitiesList.jsx
+++ fast-company/src/app/components/ui/qualities/qualitiesList.jsx
@@ -6,6 +6,7 @@ import { useQualities } from "../../../hooks/useQualities";
 const QualitiesList = ({ qualities }) => {
     const { isLoading } = useQualities();
     if (isLoading) return "Loading...";
+    console.log("qualities11111", qualities);
     return (
         <>
             {qualities.map((qual) => (
